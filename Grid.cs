ad716eab437e90e344efd493d2cdecd6a06b4a29
mario <mario201santos11@gmail.com>
2023-12-01 17:30:28 +0000

    0.01

---
+++ Grid.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace NemLinha_Projeto
+{
+    //┌ ┐ │ ├ ┤ └ ┘ ┼ ┬ ┴
+    //⏺ ◍ ⚪ ⚫
+    public class Grid
+    {
+        private string _grid;
+        private string _topLine="";
+        private string _midGridDivision="";
+        private string _botLine="";
+        public void DisplayGrid(int rows, int columns,int[,] gameGrid)
+        {
+            // Generate and display the grid
+            if (_grid is null)
+            {
+                GenerateGridBoundries(rows,columns);
+                this._grid=GenerateGrid(rows, columns,gameGrid);
+            }
+            else
+            {
+                this._grid = GenerateGrid(rows, columns,gameGrid);
+            }
+            Console.WriteLine(_grid);
+        }
+
+        private void GenerateGridBoundries(int columns, int cellWidth = 5)
+        {
+            string horizontalLine = new string('─', cellWidth);
+            
+            //Top Line of Grid
+            _topLine += $"\u250c";
+            for (int c = 0; c < columns; c++)
+            {
+                _topLine += horizontalLine;
+                if (c + 1 == columns)
+                {
+                    _topLine += $"\u2510";
+                }
+                else
+                {
+                    _topLine += $"\u252c";
+                }
+            }
+            _topLine += "\n";
+
+            //Mid Line of Grid
+            _midGridDivision += $"\u251c";
+            for (int c = 0; c < columns; c++)
+            {
+                _midGridDivision += horizontalLine;
+                if (c + 1 == columns)
+                {
+                    _midGridDivision += $"\u2524";
+                }
+                else
+                {
+                    _midGridDivision += $"\u253c";
+                }
+            }
+            _midGridDivision += "\n";
+            
+            //Bot Line of Grid
+            _botLine += $"\u2514";
+            for (int c = 0; c < columns; c++)
+            {
+                _botLine += horizontalLine;
+                if (c + 1 == columns)
+                {
+                    _botLine += $"\u2518";
+                }
+                else
+                {
+                    _botLine += $"\u2534";
+                }
+            }
+        }
+
+        public string GenerateGrid(int rows, int columns, int[,] gameGrid,int cellWidth=5)
+        {
+            string grid = "";
+            
+            string verticalLine = new string(' ', cellWidth);
+
+            grid += _topLine;
+            
+            grid+=this.Lines(verticalLine, rows, columns,gameGrid);
+            
+            return grid;
+        }
+
+        private string Lines(string verticalLine,int rows,int columns,int[,] gameGrid=null)
+        {
+            string lineRows="";
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    if (gameGrid != null)
+                        switch (gameGrid[r, c])
+                        {
+                            case 1:
+                                lineRows += "│  O  ";
+                                break;
+                            case 2:
+                                lineRows += "│  X  ";
+                                break;
+                            default:
+                                lineRows += "│" + verticalLine;
+                                break;
+                        }
+                }
+                lineRows += "│\n";
+                
+                if (r < rows - 1)
+                {
+                    lineRows+=_midGridDivision;
+                }
+                else
+                {
+                    lineRows += _botLine;
+                }
+                
+            }
+
+            return lineRows;
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace NemLinha_Projeto
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] gameGrid = { 
+                {1,2,3,4,1}, 
+                {6,7,2,1,0}, 
+                {1,2,1,2,5}, 
+                {6,1,8,9,0}, 
+                {1,2,3,4,5} };
+            Grid uIgrid = new Grid();
+            Console.Clear();
+            uIgrid.DisplayGrid(5,5,gameGrid);
+            Thread.Sleep(5000);
+            Console.Clear();
+            int[,] GameGrid2={ 
+                {1,2,3,4,1}, 
+                {6,7,2,1,0}, 
+                {1,2,1,1,5}, 
+                {6,1,8,1,0}, 
+                {1,2,3,1,5} };
+            uIgrid.DisplayGrid(5, 5, GameGrid2);
+        }
+    }
+}
