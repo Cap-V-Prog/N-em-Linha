3a22735b492478668503616feaebf72589e5a49b
mario <mario201santos11@gmail.com>
2023-12-03 19:42:03 +0000

    0.04 - Nuget packet added for JSON files

--- Player.cs
+++ Player.cs
@@ -1,7 +1,39 @@
-﻿namespace NemLinha_Projeto
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NemLinha_Projeto
 {
     public class Player
     {
+        public string Name { get; set; }
+        public int GamesPlayed { get; set; }
+        public int Victories { get; set; }
         
+        public Player(string name, int gamesPlayed, int victories)
+        {
+            Name = name;
+            GamesPlayed = gamesPlayed;
+            Victories = victories;
+        }
+
+        // Method to display player information
+        public void DisplayPlayerInfo()
+        {
+            Console.WriteLine($"Player: {Name}");
+            Console.WriteLine($"Games Played: {GamesPlayed}");
+            Console.WriteLine($"Victories: {Victories}");
+        }
+
+        // Method to convert player data to JSON string
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        // Method to create a Player object from JSON string
+        public static Player FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Player>(json);
+        }
     }
 }
---
+++ PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace NemLinha_Projeto
+{
+    public class PlayerManager
+    {
+        private const string FilePath = "players.json";
+
+        // Method to save player data to a JSON file
+        public static void SavePlayers(List<Player> players)
+        {
+            string json = JsonConvert.SerializeObject(players, Formatting.Indented);
+            File.WriteAllText(FilePath, json);
+        }
+
+        // Method to load player data from a JSON file
+        public static List<Player> LoadPlayers()
+        {
+            if (File.Exists(FilePath))
+            {
+                string json = File.ReadAllText(FilePath);
+                return JsonConvert.DeserializeObject<List<Player>>(json);
+            }
+            else
+            {
+                return new List<Player>();
+            }
+        }
+    }
+}
